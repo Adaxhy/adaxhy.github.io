edd33303f8c36634da39252ddaab562021972402
Adaxhy <147033905+Adaxhy@users.noreply.github.com>
2026-03-02 04:47:31 +0000

    Deploying to gh-pages from @ Adaxhy/Adaxhy.github.io@6000572124409fc02c2f9918e743c04507a6b09c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Adaxhy/hyxu.github.io",
+    "data-repo": "Adaxhy/Adaxhy.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
